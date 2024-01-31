3ff69628f7489a684c7f62b6afdc3754f3279999
vvkrish08 <itsvvamsi@gmail.com>
2024-01-31 16:12:55 -0600

    initial commmit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import WeatherApp from './Component/WeatherApp/WeatherApp';
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
